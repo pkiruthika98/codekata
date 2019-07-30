209f576158e4b5cda621c02631a9253c3733bcb1
pkiruthika98 <53472691+pkiruthika98@users.noreply.github.com>
2019-07-30 20:56:31 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+    {
+        printf("vowel");
+    }
+    else if(ch=='b'||ch=='c'||ch=='d'||ch=='f'||ch=='g'||ch=='h'||ch=='j'||ch=='k'||ch=='l'||ch=='m'||ch=='n'||ch=='p'||ch=='q'||ch=='r'||ch=='s'||ch=='t'||ch=='v'||ch=='w'||ch=='x'||ch=='y'||ch=='z')
+    
+    {
+        printf("consonant");
+    }
+    else
+    {
+        printf("special character");
+    }
+    return 0;
+}
