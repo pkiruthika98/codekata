d6a08e67ebd0e86a952642249c695ed320599d33
pkiruthika98 <53472691+pkiruthika98@users.noreply.github.com>
2019-07-31 14:35:05 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   int year;
+   scanf("%d",&year);
+    if(((year%100==0)&&(year%4==0))||((year%100!=0)&&(year%4==0)))
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
