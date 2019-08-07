3f45ba8dcf27f68fe04950bc3493d44ae55c89cf
pkiruthika98 <53472691+pkiruthika98@users.noreply.github.com>
2019-08-07 17:42:53 +0530

    Create wordspa.c

---
+++ wordspa.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int c=0,i,w=0,t;
+    char a[90];
+    scanf("%[^\n]",a);
+    t=strlen(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]=='.')
+        {
+            c++;
+        }
+        else
+        {
+            w++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
